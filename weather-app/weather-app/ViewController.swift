cfe10c46b029c552d297cb84e46353b18dac844b
Annie Tang <annietang@berkeley.edu>
2017-10-08 21:28:42 -0700

    initializing project

---
+++ weather-app/weather-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weather-app
+//
+//  Created by Annie Tang on 10/8/17.
+//  Copyright © 2017 Annie Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
